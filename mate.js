fbb59c1aa5e93b65d229bfcb6cb45e90069e3280
Kat Reinhart <kat.reinhart@gmail.com>
2018-02-21 15:13:50 -0800

    First pass solution

--- mate.js
+++ mate.js
@@ -1,6 +1,19 @@
 const AssertionError = require('assert').AssertionError;
 const chalk = require('chalk')
 
+function describe(message, callback) {
+  console.log(" --- ", message, " --- ")
+  callback()
+}
+
+function it(message, callback) {
+  try {
+    callback()
+    console.log(chalk.green(message), "✅")
+  } catch (e) {
+    console.log(chalk.red(e.message), "❌")
+  }
+}
 
 module.exports = {
   describe,
